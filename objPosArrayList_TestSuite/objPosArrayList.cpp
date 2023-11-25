b09e50d76a3996ecbbcb18d835751c93fdb86d12
Noah Singh <singhn98@mcmaster.ca>
2023-11-25 11:49:20 -0500

    Iteration 2A V.1

--- objPosArrayList_TestSuite/objPosArrayList.cpp
+++ objPosArrayList_TestSuite/objPosArrayList.cpp
@@ -5,50 +5,65 @@
 
 objPosArrayList::objPosArrayList()
 {
-
+    aList = new objPos[ARRAY_MAX_CAP];
+    sizeArray = ARRAY_MAX_CAP;
+    sizeList = 0;
 }
 
 objPosArrayList::~objPosArrayList()
 {
-
+    delete[] aList;
 }
 
 int objPosArrayList::getSize()
 {
-
+    return sizeList;
 }
 
 void objPosArrayList::insertHead(objPos thisPos)
 {
-
+    for (int i = sizeList; i >0; i--){
+        aList[i].setObjPos(aList[i-1]);
+    }
+    aList[0].setObjPos(thisPos);
+    sizeList++;
 }
 
 void objPosArrayList::insertTail(objPos thisPos)
 {
-
+    if(sizeList == sizeArray) {
+        //Full
+    }
+    else {
+        aList[sizeList].setObjPos(thisPos);
+        sizeList++;
+    }
 }
 
 void objPosArrayList::removeHead()
 {
-    
+    for (int i = 0; i < sizeList - 1; i++){
+        aList[i].setObjPos(aList[i+1]);
+    }
+    sizeList--;
 }
 
 void objPosArrayList::removeTail()
 {
-
+    sizeList--;
 }
 
 void objPosArrayList::getHeadElement(objPos &returnPos)
 {
-
+    returnPos.setObjPos(aList[0]);
 }
 
 void objPosArrayList::getTailElement(objPos &returnPos)
 {
-
+    returnPos.setObjPos(aList[sizeList-1]);
 }
 
 void objPosArrayList::getElement(objPos &returnPos, int index)
 {
-
+    returnPos.setObjPos(aList[index]);
 }
